1af0f92577a72da118850975b366864eb5da3c28
Fedor Ermishin <ermishinf2@gmail.com>
2016-09-23 20:13:29 +0300

    Added testing of all possible sequences below given length, fixed bug with numbers with multiple zeros

--- 2.py
+++ 2.py
@@ -24,8 +24,8 @@ def solve_fast(num):
             if s == 0:
                 tail_next = ''
             else:
-                tail_next = str(int(tail) + 1)
-            tail_next = tail_next[-s:]  # process overflow
+                tail_next = str(int(tail) + 1).zfill(s)
+            tail_next = tail_next[-s:]  # handle overflow
             pos = head + tail_next
             # verify hypothesis that num can be generated from numbers pos-1, pos, pos+1, ...
             accum = tail
@@ -61,19 +61,21 @@ def compare_solutions(solver1, solver2, max_num):
             ok = False
             break
     if ok:
-        print("Success")
+        print("Compare solutions: success")
 
 
 def run_tests(solver):
     def test(number, expected_answer):
         received_answer = solver(number)
-        if (received_answer != expected_answer):
+        if received_answer != expected_answer:
             print('Test failed!')
             print('number: {}, expected: {}, received {}'.format(number, expected_answer, received_answer))
+
     test('1', 1)
     test('0', 11)
     test('02', 31)
     test('00', 191)
+    test('001', 191)
     test('000', 2891)
     test('90', 170)
     test('991', 188)
@@ -83,17 +85,41 @@ def run_tests(solver):
     test('99', index_from_pos('89') + 1)
     test('999', index_from_pos('899') + 1)
     test('99999', index_from_pos('89999') + 1)
-    test('99999999999999999999999999999999999999999999999999', index_from_pos('89999999999999999999999999999999999999999999999999') + 1)
-    test('00000000000000000000000000000000000000000000000000', index_from_pos('100000000000000000000000000000000000000000000000000') + 1)
+    test('99999999999999999999999999999999999999999999999999',
+         index_from_pos('89999999999999999999999999999999999999999999999999') + 1)
+    test('00000000000000000000000000000000000000000000000000',
+         index_from_pos('100000000000000000000000000000000000000000000000000') + 1)
     test('00000000000000000000000000000000000000000000000000', 4988888888888888888888888888888888888888888888888891)
     test('12345678910111213141516171819202122232425262728293', 1)
 
+
+def test_sequence(solver, max_pos, max_len):
+    seq = ''.join(map(str, range(1, max_pos + 1)))
+    ok = True
+    for i in range(len(seq)):
+        for ln in range(1, max_len):
+            if i + ln < len(seq):
+                subseq = seq[i:i + ln]
+                expected_answer = i + 1
+                received_answer = solver(subseq)
+                i2 = received_answer - 1
+                if (received_answer > expected_answer or
+                                received_answer < expected_answer and seq[i2:i2 + ln] != subseq):
+                    print('Test failed!')
+                    print('number: {}, expected: {}, received {}'.format(subseq, expected_answer, received_answer))
+                    ok = False
+    if ok:
+        print('Test sequence: success')
+
+
 def main():
     # process_input()
-    # compare_solutions(solve_simple, solve_fast, 100)
-    # compare_solutions('10000')
+    # compare_solutions(solve_simple, solve_fast, 1000)
     # run_tests(solve_simple)
-    run_tests(solve_fast)
+    # run_tests(solve_fast)
+    # test_sequence(solve_simple, 100, 5)
+    test_sequence(solve_fast, 1000, 50)
+    # test_sequence(solve_fast, 10000, 10)
 
 
 if __name__ == "__main__":
