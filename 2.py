03a8d09c46fe32718d9773ec9c83e7aa33ddbc03
Fedor Ermishin <ermishinf2@gmail.com>
2016-09-23 17:34:04 +0300

    Changed type of input from int to string

--- 2.py
+++ 2.py
@@ -3,13 +3,12 @@ import math
 
 
 def solve_fast(num):
-    str_num = str(num)
-    for ln in range(1, len(str_num) + 1):  # length of solution cannot exceed length of num
+    for ln in range(1, len(num) + 1):  # length of solution cannot exceed length of num
         # select split point
         min_index = math.inf
         for s in range(0, ln):
-            tail = str_num[:s]
-            head = str_num[s:ln]
+            tail = num[:s]
+            head = num[s:ln]
             if head[0] == '0':
                 continue  # number cannot start with zero
             if s == 0:
@@ -21,10 +20,10 @@ def solve_fast(num):
             # verify hypothesis that num can be generated from numbers pos-1, pos, pos+1, ...
             accum = tail
             counter = int(pos)
-            while len(accum) < len(str_num):
+            while len(accum) < len(num):
                 accum += str(counter)
                 counter += 1
-            if accum[:len(str_num)] == str_num:
+            if accum[:len(num)] == num:
                 # compute index in string from pos
                 index = 0
                 for i in range(1, ln):
@@ -37,19 +36,19 @@ def solve_fast(num):
 
 
 def solve_simple(num):
-    text = ''.join(map(str, range(1, pow(10, len(str(num))) + 1)))
-    # text = ''.join(map(str, range(1, num + 1)))
-    return text.find(str(num)) + 1  # indexing starts from 1
+    text = ''.join(map(str, range(1, pow(10, len(num)) + 1)))
+    # text = ''.join(map(str, range(1, int(num) + 1)))
+    return text.find(num) + 1  # indexing starts from 1
 
 
 def process_input():
     for line in sys.stdin.readlines():
-        print(solve_simple(int(line)))
+        print(solve_simple(line))
 
 
 def compare_solutions(solver1, solver2, max_num):
     ok = True
-    for num in range(1, max_num):
+    for num in map(str, range(1, max_num)):
         answer1 = solver1(num)
         answer2 = solver2(num)
         if answer1 != answer2:
@@ -67,19 +66,21 @@ def run_tests(solver):
         if (received_answer != expected_answer):
             print('Test failed!')
             print('number: {}, expected: {}, received {}'.format(number, expected_answer, received_answer))
-    test(1, 1)
-    test(0, 11)
-    test(90, 170)
-    test(991, 188)
-    test(9100, 189)
-    test(6789, 6)
-    test(111, 12)
+    test('1', 1)
+    test('0', 11)
+    test('02', 31)
+    test('00', 191)
+    test('90', 170)
+    test('991', 188)
+    test('9100', 189)
+    test('6789', 6)
+    test('111', 12)
 
 
 def main():
     # process_input()
     # compare_solutions(solve_simple, solve_fast, 100)
-    # compare_solutions(10000)
+    # compare_solutions('10000')
     run_tests(solve_simple)
     run_tests(solve_fast)
 
