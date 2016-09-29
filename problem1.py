ceae77efedb6611b4bc7bd6763b8013312628fc1
Fedor Ermishin <ermishinf2@gmail.com>
2016-09-29 12:29:26 +0300

    Added testing for symmetry

--- problem1.py
+++ problem1.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def dijkstra(ground, y0, x0):
-    # fill the island starting from outside
+    # fill the island with water starting from outside
     (n, m) = ground.shape
     water = ground.copy()
     u = np.zeros((n, m), dtype=bool)
@@ -16,27 +16,31 @@ def dijkstra(ground, y0, x0):
         (_, (y, x)) = heapq.heappop(q)
         for d in dv:
             (y2, x2) = (y + d[0], x + d[1])
-            if y2 >= 0 and x2 >= 0 and y2 < n and x2 < m:
+            if 0 <= y2 < n and 0 <= x2 < m:
                 if ~u[y2, x2]:
-                    if (water[y2, x2] < water[y, x]):
+                    if water[y2, x2] < water[y, x]:
                         water[y2, x2] = water[y, x]
                     u[y2, x2] = True
                     heapq.heappush(q, (water[y2, x2], (y2, x2)))
     return np.sum(water - ground)
 
 
-def solve(input, output):
-    n_tests = int(input.readline())
+def solve_test_case(n, m, ground):
+    # extend boundaries with zeros
+    b = np.zeros((n + 2, m + 2), dtype=int)
+    b[1:n+1, 1:m+1] = np.array(ground, dtype=int)
+    return dijkstra(b, 0, 0)
+
+
+def solve(in_stream, out_stream):
+    n_tests = int(in_stream.readline())
     for _ in range(n_tests):
         # read the matrix
-        (n, m) = map(int, input.readline().split())
-        a = [None] * n
+        (n, m) = map(int, in_stream.readline().split())
+        ground = [None] * n
         for i in range(n):
-            a[i] = list(map(int, input.readline().split()))
-        # extend boundaries with zeros
-        b = np.zeros((n + 2, m + 2), dtype=int)
-        b[1:n+1, 1:m+1] = np.array(a, dtype=int)
-        print(dijkstra(b, 0, 0), file=output)
+            ground[i] = list(map(int, in_stream.readline().split()))
+        print(solve_test_case(n, m, ground), file=out_stream)
 
 
 def main():
--- problem1_test.py
+++ problem1_test.py
@@ -1,40 +1,63 @@
 import numpy as np
-from problem1 import solve
+
+from problem1 import solve, solve_test_case
 
 
 def compare(file1, file2):
     return list(map(int, file1.readlines())) == list(map(int, file2.readlines()))
 
+
 def generate_test():
     a = np.zeros((50, 50))
     for i in range(12):
-        a[:,i*4:i*4+3] = 5
+        a[:, i * 4:i * 4 + 3] = 5
 
     for i in range(6):
-        a[0:3,i*8+3] = 5
-        a[-3:,i*8+7] = 5
+        a[0:3, i * 8 + 3] = 5
+        a[-3:, i * 8 + 7] = 5
 
     for i in range(12):
-        a[1:-1,i*4+1] = 3
+        a[1:-1, i * 4 + 1] = 3
 
     for i in range(6):
-        a[1,i*8+2:i*8+5] = 3
-        a[-2,i*8+6:i*8+9] = 3
+        a[1, i * 8 + 2:i * 8 + 5] = 3
+        a[-2, i * 8 + 6:i * 8 + 9] = 3
 
     with open('temp.txt', 'wb') as f:
         np.savetxt(f, a, fmt='%.0f')
 
 
-def main():
-    with open('input.txt', 'rt') as input:
-        with open('output.txt', 'wt') as output:
-            solve(input, output)
-    with open('output.txt', 'rt') as output:
-        with open('answers.txt', 'rt') as answers:
-            if compare(output, answers):
-                print('Success')
+def test_symmetry():
+    ok = True
+    np.random.seed(12345)
+    for i in range(50):
+        n = np.random.randint(1, 50 + 1)
+        m = np.random.randint(1, 50 + 1)
+        a = np.random.randint(1, 1000 + 1, (n, m))
+        ans1 = solve_test_case(n, m, a)
+        ans2 = solve_test_case(m, n, a.transpose())
+        if ans1 != ans2:
+            print('Answers are different!')
+            ok = False
+    if ok:
+        print('Test symmetry: success')
+
+
+def run_tests():
+    with open('input.txt', 'rt') as in_stream:
+        with open('output.txt', 'wt') as out_stream:
+            solve(in_stream, out_stream)
+    with open('output.txt', 'rt') as out_stream:
+        with open('answers.txt', 'rt') as ans_stream:
+            if compare(out_stream, ans_stream):
+                print('Run tests: success')
             else:
                 print('Wrong output!')
+
+
+def main():
+    test_symmetry()
+    run_tests()
 
 
 if __name__ == '__main__':
